2ba288da487387d85a965b9be53eda88b5815ebe
dominikasienko <133100597+dominikasienko@users.noreply.github.com>
2025-05-12 14:48:10 +0200

    Add RecipeApp/Models/NutritionalInfo.swift 1747054090187

--- RecipeApp/Models/NutritionalInfo.swift
+++ RecipeApp/Models/NutritionalInfo.swift
@@ -1,8 +1,24 @@
 import Foundation
 
-struct NutritionalInfo: Codable {
+struct NutritionalInfo: Codable, Identifiable {
+    let id: String
     var calories: Double
     var protein: Double
+    var carbs: Double
     var fat: Double
-    var carbohydrates: Double
+    var fiber: Double
+    
+    init(id: String = UUID().uuidString,
+         calories: Double,
+         protein: Double,
+         carbs: Double,
+         fat: Double,
+         fiber: Double) {
+        self.id = id
+        self.calories = calories
+        self.protein = protein
+        self.carbs = carbs
+        self.fat = fat
+        self.fiber = fiber
+    }
 }
